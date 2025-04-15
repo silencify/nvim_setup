192c4c2ab4edd3fc1d225768ebea1b799254de93
silencify <rojen.maharjan@protonmail.com>
2025-04-15 23:43:38 +0545

    Move config code

--- nvim/lua/plugin_config/nvim-tree.lua
+++ nvim/lua/plugin_config/nvim-tree.lua
@@ -3,9 +3,6 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
 -- empty setup using defaults
 require("nvim-tree").setup({
   sort = {
